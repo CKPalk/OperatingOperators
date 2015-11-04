d031fe39b54ede6afc594958d2434a884439a07d
Cameron Palk <ckpalk@comcast.net>
2015-11-03 21:14:17 -0800

    Added all files to repo

---
+++ OperatingOperators/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OperatingOperators
+//
+//  Created by Cameron Palk on 11/3/15.
+//  Copyright © 2015 Cameron Palk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
